85a0be99d5ccc5fae7f125a87d17d72d8b18ab8c
Robert <chaolong_chen@hotmail.com>
2018-08-21 23:00:07 +0800

    [LeetCode 220] Contains Duplicate III

---
+++ ex220ContainsDuplicateIII/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex220ContainsDuplicateIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.ContainsNearbyAlmostDuplicate(new int[]{ 1, 2, 3, 1}, 3, 0));
+            Console.WriteLine(solution.ContainsNearbyAlmostDuplicate(new int[]{ 1, 0, 1, 1}, 1, 2));
+            Console.WriteLine(solution.ContainsNearbyAlmostDuplicate(new int[]{ 1, 5, 9, 1, 5, 9}, 2, 3));
+            Console.ReadLine();   
+        }
+    }
+}
---
+++ ex220ContainsDuplicateIII/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex220ContainsDuplicateIII
+{
+    public class Solution
+    {
+        /*
+         * Condition : Abs(i - j) < k && Abs(num[i] - num[j]) < t
+         */
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            var bucket = new Dictionary<long, long>();
+
+            if (t < 0 || k < 0 || nums.Length <= 0) return false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var bucketIndex = ((long)nums[i] - Int32.MinValue) / ((long)t + 1);
+                if (i - k - 1 >= 0) {
+                    bucket.Remove(((long)nums[i - k -1] - Int32.MinValue)/((long)t+1));
+                }
+
+                if (bucket.ContainsKey(bucketIndex)
+                    || bucket.ContainsKey(bucketIndex - 1) && nums[i] - bucket[bucketIndex - 1] <= t
+                    || bucket.ContainsKey(bucketIndex + 1) && bucket[bucketIndex + 1] - nums[i] <= t
+                    )
+                    return true;
+                bucket[bucketIndex] = nums[i];
+            }
+            return false;
+        }
+    }
+}
