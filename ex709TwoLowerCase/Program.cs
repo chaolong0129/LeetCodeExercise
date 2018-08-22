e43d273386e8b20b16d9b04beddf430813c72519
Robert <chaolong_chen@hotmail.com>
2018-08-22 22:21:32 +0800

    [LeetCode] To Lower Case

---
+++ ex709TwoLowerCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex709ToLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "HelloWorld";
+            var solution = new Solution();
+            Console.WriteLine(solution.ToLowerCase(str));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex709TwoLowerCase/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex709ToLowerCase
+{
+    public class Solution
+    {
+        public string ToLowerCase(string str)
+        {
+            var cstr = str.ToCharArray();
+            for (int i = 0; i < cstr.Length; i++)
+            {
+                if (cstr[i] > 64 &&cstr[i] < 91)
+                    cstr[i] = (char)(cstr[i] + 32);
+            }
+            return new string(cstr);
+        }
+    }
+}
