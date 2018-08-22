3023e994d6c171a453180364a1a294ceda46f091
Robert Chen <robert.chen@fitivision.com>
2018-08-22 17:01:53 +0800

    [LeetCode 653] Two Sum - Input is a BST.

---
+++ ex653TwoSumAndBST/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex653TwoSumAndBST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var ret = solution.FindTarget(new TreeNode(5)
+            {
+                left = new TreeNode(3) { left = new TreeNode(2), right = new TreeNode(4) },
+                right = new TreeNode(6) { right = new TreeNode(7) }
+            }, 9);
+            Console.WriteLine(ret);
+            //var ret1 = solution.FindTarget(new TreeNode(2)
+            //, 1);
+            //Console.WriteLine(ret1);
+
+            //var ret2 = solution.FindTarget(new TreeNode(2) { right = new TreeNode(3) }, 6);
+            //Console.WriteLine(ret2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex653TwoSumAndBST/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex653TwoSumAndBST
+{
+    /**
+    * Definition for a binary tree node.
+    * public class TreeNode {
+    *     public int val;
+    *     public TreeNode left;
+    *     public TreeNode right;
+    *     public TreeNode(int x) { val = x; }
+    * }
+    */
+    public class Solution
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            var list = new List<int>();
+
+            BST2LSTMID(root, list);
+            list.Sort();
+            if (list.Count < 2) return false;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int complement = k - list[i];
+                if (list.Contains(complement) && list[i] != complement) return true;
+                continue;
+            }
+            return false;
+        }
+
+        //post-order
+        private void BST2LSTPOST(TreeNode root, List<int> list)
+        {
+            if (root.left != null) BST2LSTPOST(root.left, list);
+            if (root.right != null) BST2LSTPOST(root.right, list);
+            if (root != null)
+                list.Add(root.val);
+        }
+
+        //pre-order
+        private void BST2LSTPRE(TreeNode root, List<int> list)
+        {
+            if (root != null)
+                list.Add(root.val);
+            if (root.left != null) BST2LSTPRE(root.left, list);
+            if (root.right != null) BST2LSTPRE(root.right, list);
+        }
+
+        // mid-order
+        private void BST2LSTMID(TreeNode root, List<int> list)
+        {
+            if (root.left != null) BST2LSTMID(root.left, list);
+            if (root != null)
+                list.Add(root.val);
+            if (root.right != null) BST2LSTMID(root.right, list);
+        }
+    }
+}
