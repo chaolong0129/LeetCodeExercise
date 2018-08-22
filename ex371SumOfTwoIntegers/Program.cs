4587ae9084c115a826f22f766af10463abda4513
Robert <chaolong_chen@hotmail.com>
2018-08-22 21:55:50 +0800

    [LeetCode] Sum of Two Integers

---
+++ ex371SumOfTwoIntegers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex371SumOfTwoIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.GetSum(1, 2));
+            Console.WriteLine(solution.GetSum(-1, -1));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex371SumOfTwoIntegers/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex371SumOfTwoIntegers
+{
+    public class Solution
+    {
+        public int GetSum(int a, int b)
+        {
+            if (a == 0)
+                return b;
+            if (b == 0)
+                return a;
+
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+
+            return GetSum(sum, carry);
+        }
+    }
+}
