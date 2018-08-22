26cb8dd61f7798f65c5c4d29561fbee15aa0c3d0
Robert <chaolong_chen@hotmail.com>
2018-08-22 21:29:52 +0800

    [LeetCode 002] Add Two Numbers - Input are ListNodes

---
+++ ex002AddTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex002AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            ListNode one = new ListNode(2) {
+                next = new ListNode(4) {
+                    next = new ListNode(3)
+                }
+            };
+            ListNode two = new ListNode(5)
+            {
+                next = new ListNode(6)
+                {
+                    next = new ListNode(4)
+                }
+            };
+            var ret = solution.AddTwoNumbers(one, two);
+            while (ret != null) {
+                Console.WriteLine(ret.val);
+                ret = ret.next;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex002AddTwoNumbers/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex002AddTwoNumbers
+{
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode one = l1, two = l2;
+            ListNode ret = new ListNode(0);
+            ListNode three = ret;
+            int sum = 0;
+
+            while (one != null && two != null)
+            {
+                var d = (one.val + two.val + sum) % 10;
+                sum = (one.val + two.val + sum) / 10;
+                three.next = new ListNode(d);
+                one = one.next;
+                two = two.next;
+                three = three.next;
+            }
+
+            while (one != null)
+            {
+                var d = (one.val + sum) % 10;
+                sum = (one.val + sum) / 10;
+                three.next = new ListNode(d);
+                one = one.next;
+                three = three.next;
+            }
+
+            while (two != null)
+            {
+                var d = (two.val + sum) % 10;
+                sum = (two.val + sum) / 10;
+                three.next = new ListNode(d);
+                two = two.next;
+                three = three.next;
+            }
+
+            if (sum != 0)
+                three.next = new ListNode(1);
+
+            return ret.next;
+        }
+    }
+}
