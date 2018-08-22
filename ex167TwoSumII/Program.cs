3d0c57d80e4e5f86d4b57752a167a1c3b04efb5a
Robert Chen <robert.chen@fitivision.com>
2018-08-22 13:23:48 +0800

    [LeetCode 167] Two Sum ||

---
+++ ex167TwoSumII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex167TwoSumII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var ret = solution.TwoSum(new int[] { 2, 7, 1, 3 }, 9);
+            foreach (var idx in ret)
+            {
+                Console.Write($"{idx} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex167TwoSumII/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex167TwoSumII
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int f = 0, l = numbers.Length - 1;
+            while (f < l) {
+                int sum = numbers[f] + numbers[l];
+                if (sum == target) return new int[] { f + 1, l + 1 };
+                else if (sum < target) ++f;
+                else --l;
+            }
+            throw new Exception("Not found!");
+        }
+    }
+}
