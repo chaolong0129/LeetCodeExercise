a4496a58e3d3b85857eadcd96c1e8180518dcdfd
Robert <chaolong_chen@hotmail.com>
2018-08-22 08:35:26 +0800

    [LeetCode 001] Two Sum

---
+++ ex001TwoSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex001TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var ret = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            //var ret2 = solution.TwoSum2(new int[] { 2, 7, 11, 15 }, 9);
+            var ret2 = solution.TwoSum2(new int[] { 3, 3}, 6);
+            foreach (var n in ret)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+            foreach (var n in ret2)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex001TwoSum/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex001TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length -1; i++) {
+                for (int j = i + 1; j < nums.Length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            throw new Exception("Not Found!");
+        }
+
+        public int[] TwoSum2(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (dict.ContainsValue(complement))
+                {
+                    var key = dict.FirstOrDefault(k => k.Value == complement).Key;
+                    if (key != i)
+                        return new int[] { key, i };
+                }
+                dict.Add(i, nums[i]);
+            }
+            
+            throw new Exception("Not Found!");
+        }
+    }
+}
