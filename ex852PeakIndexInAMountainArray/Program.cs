55d5f723616bd38bde58eaa2ce7f53840abdb33f
Robert Chen <robert.chen@fitivision.com>
2018-08-23 17:35:23 +0800

    [LeetCode 825] Peak Index In A Mountain Array

---
+++ ex852PeakIndexInAMountainArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex852PeakIndexInAMountainArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.PeakIndexInMountainArray(new int[] { 0, 1, 0}));
+            Console.WriteLine(solution.PeakIndexInMountainArray(new int[] { 0, 2, 1, 0 }));
+            Console.WriteLine(solution.PeakIndexInMountainArray2(new int[] { 0, 1, 0 }));
+            Console.WriteLine(solution.PeakIndexInMountainArray2(new int[] { 0, 2, 1, 0 }));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex852PeakIndexInAMountainArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex852PeakIndexInAMountainArray
+{
+    public class Solution
+    {
+        public int PeakIndexInMountainArray(int[] A)
+        {
+            int idx = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                var cMnt = A[i];
+                for (int j = i+1; j < A.Length; j++)
+                {
+                    if (cMnt < A[j]) {
+                        idx = j;
+                        cMnt = A[j];
+                    }
+                }
+            }
+            return idx;
+        }
+    }
+}
