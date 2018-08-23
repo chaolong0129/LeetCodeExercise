2548df83492e5bc7ccf4b5b3a16c23c01b2abb47
Robert Chen <robert.chen@fitivision.com>
2018-08-23 12:04:40 +0800

    [LeetCode] Number Of 1 Bits

---
+++ ex191NumberOf1Bits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex191NumberOf1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.HammingWeight(11));
+            Console.WriteLine(solution.HammingWeight(128));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex191NumberOf1Bits/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex191NumberOf1Bits
+{
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            var nstr = Convert.ToString(n, 2);
+            int dist = 0;
+            foreach (var d in nstr)
+                if (d == '1') dist++;
+            return dist;
+        }
+    }
+}
