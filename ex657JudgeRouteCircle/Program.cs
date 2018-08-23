181fbecb6756a6019ca47160cf9c4129812259f7
Robert Chen <robert.chen@fitivision.com>
2018-08-23 11:08:39 +0800

    [LeetCode 657] Judge Route Circle

---
+++ ex657JudgeRouteCircle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex657JudgeRouteCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.JudgeCircle("UD"));
+            Console.WriteLine(solution.JudgeCircle("LL"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex657JudgeRouteCircle/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex657JudgeRouteCircle
+{
+    public class Solution
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0, y = 0;
+
+            foreach (var m in moves)
+            {
+                switch (m) {
+                    case 'U': y++; break;
+                    case 'D': y--; break;
+                    case 'R': x++; break;
+                    case 'L': x--; break;
+                    default:break;
+                }
+            }
+
+            return (x == 0 && y == 0);
+        }
+    }
+}
