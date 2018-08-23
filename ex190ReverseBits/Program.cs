46b148c5aa35b7461aca2a0d60b5033086c5949e
Robert Chen <robert.chen@fitivision.com>
2018-08-23 14:18:49 +0800

    [LeetCode 190] Reverse Bits

---
+++ ex190ReverseBits/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex190ReverseBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.reverseBits(43261596));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex190ReverseBits/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex190ReverseBits
+{
+    public class Solution
+    {
+        // Use operator >>, << to do shift and use & operator to got vlaue;
+        public uint reverseBits(uint n)
+        {
+            uint ret = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                var iBits = (n >> i) & 1;
+                ret = (ret << 1) + iBits;
+            }
+            return ret;
+        }
+    }
+}
