1364534a452450c7ba84942b929f75a0da09e245
Robert Chen <robert.chen@fitivision.com>
2018-08-23 15:41:16 +0800

    [LeetCode 804] Unique Morse Code Words

---
+++ ex804UniqueMorseCodeWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex804UniqueMorseCodeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.UniqueMorseRepresentations(new string[] { "gin", "zen", "gig", "msg" }));
+            Console.WriteLine(solution.UniqueMorseRepresentations(new string[] { "aittjje", "auyyn", "lqtktn", "lmjwn" }));
+            Console.WriteLine(solution.UniqueMorseRepresentations(new string[] { "aittjje"}));
+
+            Console.WriteLine(solution.UniqueMorseRepresentations2(new string[] { "gin", "zen", "gig", "msg" }));
+            Console.WriteLine(solution.UniqueMorseRepresentations2(new string[] { "aittjje", "auyyn", "lqtktn", "lmjwn" }));
+            Console.WriteLine(solution.UniqueMorseRepresentations2(new string[] { "aittjje" }));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex804UniqueMorseCodeWords/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex804UniqueMorseCodeWords
+{
+    public class Solution
+    {
+
+        string[] MCode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            HashSet<string> ret = new HashSet<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var w = words[i].ToLower();
+                var nw = "";
+                foreach (var c in w)
+                    nw = nw + MCode[(c - 97)];
+                ret.Add(nw);
+            }
+
+            return ret.Count;
+        }
+        public int UniqueMorseRepresentations2(string[] words)
+        {
+            List<char> listAToZ = Enumerable.Range('a', 26)
+                                        .Select(x => (char)x)
+                                        .ToList();
+
+            var morseCodeAns = words.Select(x =>
+            {
+                var temp = x.Select(y => MCode[listAToZ.IndexOf(y)]);
+                return string.Join(string.Empty, temp);
+            });
+
+            return morseCodeAns.Distinct().Count();
+        }
+    }
+}
