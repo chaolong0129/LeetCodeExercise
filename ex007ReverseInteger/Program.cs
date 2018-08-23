3e8b56dc138c63355d4a40183a6640392b81f9fa
Robert Chen <robert.chen@fitivision.com>
2018-08-23 14:54:48 +0800

    [LeetCode 007] Reverse Integer

---
+++ ex007ReverseInteger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex007ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.Reverse(123));
+            Console.WriteLine(solution.Reverse(-123));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex007ReverseInteger/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex007ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            int ret = 0;
+            while (x != 0) {
+                var val = x % 10;
+                var t = ret * 10 + val;
+                // To Valify the value is or not overflow.
+                if ((t - val) / 10 != ret) return 0;
+                ret = t;
+                x = x / 10;
+            }
+            return ret;
+        }
+    }
+}
