36918de27f5b384a49d31e98e8d28f2a353cb150
Robert Chen <robert.chen@fitivision.com>
2018-08-23 17:57:29 +0800

    [LeetCode 226] Invert Binary Tree

---
+++ ex226InvertBinaryTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex226InvertBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new TreeNode(4) {
+                left = new TreeNode(2) {
+                    left = new TreeNode(1),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(7) {
+                    left = new TreeNode(6),
+                    right = new TreeNode(9)
+                }
+            };
+            var solution = new Solution();
+            solution.InvertTree(tree);
+        }
+    }
+}
---
+++ ex226InvertBinaryTree/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex226InvertBinaryTree
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            var l = root.left;
+            var r = root.right;
+            root.left = InvertTree(r);
+            root.right = InvertTree(l);
+            return root;
+        }
+    }
+}
