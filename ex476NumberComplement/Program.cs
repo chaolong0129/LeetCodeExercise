ee9fe780ec39056845785095263574dfc8895f8e
Robert Chen <robert.chen@fitivision.com>
2018-08-24 11:09:50 +0800

    [LeetCode 476] Number Complement

---
+++ ex476NumberComplement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex476NumberComplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.FindComplement(5));
+            Console.WriteLine(solution.FindComplement(1));
+            Console.WriteLine(solution.FindComplement(2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex476NumberComplement/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex476NumberComplement
+{
+    public class Solution
+    {
+        public int FindComplement(int num)
+        {
+            var bnum = Convert.ToString(num, 2);
+            int result = 0;
+            for (var i = 0; i < bnum.Length; i++) {
+                var b = 1 - bnum[i] & 0x0f;
+                result = b + result*2;
+            }
+            return result;
+        }
+    }
+}
