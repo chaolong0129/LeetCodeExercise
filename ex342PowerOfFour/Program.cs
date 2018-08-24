aff3c4b3a8b546bfe15c66d40f21ba1320c24ff6
Robert Chen <robert.chen@fitivision.com>
2018-08-24 13:52:41 +0800

    [LeetCode 342] Is Power of Four

---
+++ ex342PowerOfFour/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex342PowerOfFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.IsPowerOfFourWithLoop(16));
+            Console.WriteLine(solution.IsPowerOfFourWithLoop(3));
+            Console.WriteLine(solution.IsPowerOfFourWithLoop(2));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive(16));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive(3));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive(2));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive2(16));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive2(3));
+            Console.WriteLine(solution.IsPowerOfFourWithRecursive2(2));
+            Console.WriteLine(solution.IsPowerOfFour(16));
+            Console.WriteLine(solution.IsPowerOfFour(3));
+            Console.WriteLine(solution.IsPowerOfFour(2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex342PowerOfFour/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex342PowerOfFour
+{
+    public class Solution
+    {
+        // Loop
+        public bool IsPowerOfFourWithLoop(int num)
+        {
+            while (num != 0 && (num % 4 == 0))
+                num /= 4;
+            return num == 1;
+        }
+
+        // Recursive
+        public bool IsPowerOfFourWithRecursive(int num)
+        {
+            if (num != 0 && num % 4 == 0)
+                IsPowerOfFourWithRecursive(num /= 4);
+            if (num >= 4)
+                IsPowerOfFourWithRecursive(num /= 4);
+            return (num == 1);
+        }
+
+        public bool IsPowerOfFourWithRecursive2(int num)
+        {
+            if (num > 4)
+            {
+                num = (num >> 2) / 4;
+                IsPowerOfFour(num);
+            } 
+
+            return num == 1;
+        }
+
+        public bool IsPowerOfFour(int num)
+        {
+            return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+        }
+    }
+}
